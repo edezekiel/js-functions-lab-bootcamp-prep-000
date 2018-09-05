6e3c574468893107062de76a32895de8823fc7cc
ed.a.ezekiel@gmail.com <ed.a.ezekiel@gmail.com>
2018-09-05 13:00:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountDown(){
-  
+function holidayCountDown(holiday, days){
+  return `It\'s ${days} days until ${holiday}!`
+
 }
